22d213cf33f2c89785034ed8803c1cf5cdc352bb
ProNoob1029 <pronoob1029@gmail.com>
2024-12-05 14:38:09 +0200

    fixed gherla config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/config/GherlaConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/config/GherlaConfig.kt
@@ -3,43 +3,46 @@ package org.firstinspires.ftc.teamcode.teleop.config
 import org.firstinspires.ftc.teamcode.library.config.IMUConfig
 import org.firstinspires.ftc.teamcode.library.config.MotorConfig
 import org.firstinspires.ftc.teamcode.library.config.ServoConfig
+import org.firstinspires.ftc.teamcode.robot.DriveConfig
 import org.firstinspires.ftc.teamcode.robot.RobotConfig
 
 val robotConfigGherla = RobotConfig(
-    configName = "Config Gherla",
-    imu = IMUConfig(
-        deviceName = "imu",
-        logoDirection = IMUConfig.LogoDirection.LEFT,
-        usbDirection = IMUConfig.USBDirection.UP,
-        resetYaw = true
-    ),
-    motorRF = MotorConfig(
-        deviceName = "motorRF",
-        direction = MotorConfig.Direction.FORWARD,
-        runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
-        zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
-        resetEncoder = true
-    ),
-    motorRB = MotorConfig(
-        deviceName = "motorRB",
-        direction = MotorConfig.Direction.FORWARD,
-        runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
-        zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
-        resetEncoder = true
-    ),
-    motorLF = MotorConfig(
-        deviceName = "motorLF",
-        direction = MotorConfig.Direction.REVERSE,
-        runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
-        zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
-        resetEncoder = true
-    ),
-    motorLB = MotorConfig(
-        deviceName = "motorLB",
-        direction = MotorConfig.Direction.REVERSE,
-        runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
-        zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
-        resetEncoder = true
+    name = "Config Gherla",
+    drive = DriveConfig(
+        imu = IMUConfig(
+            deviceName = "imu",
+            logoDirection = IMUConfig.LogoDirection.LEFT,
+            usbDirection = IMUConfig.USBDirection.UP,
+            resetYaw = true
+        ),
+        motorRF = MotorConfig(
+            deviceName = "motorRF",
+            direction = MotorConfig.Direction.FORWARD,
+            runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
+            zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
+            resetEncoder = true
+        ),
+        motorRB = MotorConfig(
+            deviceName = "motorRB",
+            direction = MotorConfig.Direction.FORWARD,
+            runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
+            zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
+            resetEncoder = true
+        ),
+        motorLF = MotorConfig(
+            deviceName = "motorLF",
+            direction = MotorConfig.Direction.REVERSE,
+            runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
+            zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
+            resetEncoder = true
+        ),
+        motorLB = MotorConfig(
+            deviceName = "motorLB",
+            direction = MotorConfig.Direction.REVERSE,
+            runMode = MotorConfig.RunMode.RUN_WITHOUT_ENCODER,
+            zeroPowerBehavior = MotorConfig.ZeroPowerBehavior.BRAKE,
+            resetEncoder = true
+        )
     ),
     motorLiftLeft = MotorConfig(
         deviceName = "liftLeft",
