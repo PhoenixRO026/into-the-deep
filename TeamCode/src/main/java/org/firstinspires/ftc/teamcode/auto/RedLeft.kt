add14a86ed63658d66258c48668932922ea41811
Coman <comanrazvanioan@gmail.com>
2025-01-10 21:04:34 +0200

    Started RedLeft

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedLeft.kt
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode.auto
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.canvas.Canvas
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.lib.roadrunner_ext.ex
+import com.lib.units.Pose
+import com.lib.units.deg
+import com.lib.units.inch
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.library.TimeKeep
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDriveEx
+import org.firstinspires.ftc.teamcode.robot.Robot
+import org.firstinspires.ftc.teamcode.tele.config.robotHardwareConfigWonder
+import org.firstinspires.ftc.teamcode.tele.values.robotValuesWonder
+
+@Autonomous
+class RedLeft : LinearOpMode() {
+    private val startPose = Pose(10.0.inch, -57.0.inch, 90.0.deg)
+    private val submerssible = Pose(10.0.inch, -36.0.inch, 90.0.deg)
+    private val take_specimen = Pose(38.0.inch, -50.0.inch, 0.0.deg)
+
+    override fun runOpMode() {
+        val config = robotHardwareConfigWonder
+        val values = robotValuesWonder
+
+        val dash = FtcDashboard.getInstance()
+        telemetry = MultipleTelemetry(telemetry, dash.telemetry)
+
+        telemetry.addData("Config name", config.name)
+        telemetry.addLine("INITIALIZING")
+        telemetry.update()
+
+        val timeKeep = TimeKeep()
+        val robot = Robot(hardwareMap, config, values, timeKeep)
+        val mecanumDrive = MecanumDriveEx(hardwareMap, startPose.pose2d)
+
+        val action = mecanumDrive.actionBuilder(startPose.pose2d).ex()
+            .setTangent(Math.toRadians(90.0))
+            .lineToY(-40.0)
+
+            //.afterTime(0.0, functions.scoreSecondBar())
+            .waitSeconds(1.0)
+            .setTangent(Math.toRadians(0.0))
+            .lineToXLinearHeading(34.0,Math.toRadians(45.0))
+            //.afterTime(0.0, functions.grab())
+            .waitSeconds(1.0)
+            .turnTo(Math.toRadians(-50.0))
+            //.afterTime(0.0, functions.release())
+            .waitSeconds(1.0)
+            .setTangent(Math.toRadians(0.0))
+            .lineToXLinearHeading(40.0,Math.toRadians(35.0))
+            //.afterTime(0.0, functions.grab())
+            .waitSeconds(1.0)
+            .turnTo(Math.toRadians(-50.0))
+            //.afterTime(0.0, functions.release())
+            .waitSeconds(1.0)
+            .setTangent(0.0)
+            .lineToXLinearHeading(50.0,Math.toRadians(35.0))
+            //.afterTime(0.0, functions.grab())
+            .waitSeconds(1.0)
+            .turnTo(Math.toRadians(-90.0))
+            .setTangent(Math.toRadians(180.0))
+            .lineToX(38.0)
+            //.wallGrab(arm, claw)
+            .strafeTo(take_specimen.position)
+            .waitSeconds(1.0)
+            //.afterTime(0.0, functions.scoreSecondBar())
+            .strafeTo(submerssible.position)
+            .waitSeconds(1.0)
+            //.wallGrab(arm, claw)
+            .strafeTo(take_specimen.position)
+            //.afterTime(0.0, functions.scoreSecondBar())
+            .strafeTo(submerssible.position)
+            //.wallGrab(arm, claw)
+            .strafeTo(take_specimen.position)
+            //.afterTime(0.0, functions.scoreSecondBar())
+            .strafeTo(submerssible.position)
+            .build()
+
+        telemetry.addData("Config name", config.name)
+        telemetry.addLine("READY!")
+        telemetry.update()
+
+        waitForStart()
+
+        val canvas = Canvas()
+        action.preview(canvas)
+
+        var running = true
+
+        while (isStarted && !isStopRequested && running) {
+            timeKeep.resetDeltaTime()
+
+            val packet = TelemetryPacket()
+            packet.fieldOverlay().operations.addAll(canvas.operations)
+
+            running = action.run(packet)
+            dash.sendTelemetryPacket(packet)
+
+            robot.update()
+
+            robot.addTelemetry(telemetry)
+            telemetry.update()
+        }
+    }
+}
