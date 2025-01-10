c7a6b4eef41faee8d3d383400cb820dab0283915
Coman <comanrazvanioan@gmail.com>
2025-01-10 13:19:54 +0200

    derived dragos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedRight.kt
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.auto
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.canvas.Canvas
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.lib.roadrunner_ext.ex
+import com.lib.units.Pose
+import com.lib.units.Pose2d
+import com.lib.units.deg
+import com.lib.units.inch
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.library.TimeKeep
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDriveEx
+import org.firstinspires.ftc.teamcode.robot.Robot
+import org.firstinspires.ftc.teamcode.tele.config.robotHardwareConfigWonder
+import org.firstinspires.ftc.teamcode.tele.values.robotValuesWonder
+
+@Autonomous
+class RedRight : LinearOpMode() {
+    private val startPose = Pose(0.0.inch, 0.0.inch, 0.0.deg)
+
+    override fun runOpMode() {
+        val config = robotHardwareConfigWonder
+        val values = robotValuesWonder
+
+        val dash = FtcDashboard.getInstance()
+        telemetry = MultipleTelemetry(telemetry, dash.telemetry)
+
+        telemetry.addData("Config name", config.name)
+        telemetry.addLine("INITIALIZING")
+        telemetry.update()
+
+        val startPose = Pose2d(10.0.inch, 0.0.inch, 90.0.deg)
+        val submerssible = Pose2d(10.0.inch,-36.0.inch, 90.0.deg)
+        val take_specimen = Pose2d(38.0.inch,-50.0.inch, 0.0.deg)
+
+        val timeKeep = TimeKeep()
+        val robot = Robot(hardwareMap, config, values, timeKeep)
+        val mecanumDrive = MecanumDriveEx(hardwareMap, startPose.pose2d)
+
+        val action = mecanumDrive.actionBuilder(startPose.pose2d).ex()
+            .lineToX(20.inch)
+            .build()
+
+        telemetry.addData("Config name", config.name)
+        telemetry.addLine("READY!")
+        telemetry.update()
+
+        waitForStart()
+
+        val canvas = Canvas()
+        action.preview(canvas)
+
+        var running = true
+
+        while (isStarted && !isStopRequested && running) {
+            timeKeep.resetDeltaTime()
+
+            val packet = TelemetryPacket()
+            packet.fieldOverlay().operations.addAll(canvas.operations)
+
+            running = action.run(packet)
+            dash.sendTelemetryPacket(packet)
+
+            robot.update()
+
+            robot.addTelemetry(telemetry)
+            telemetry.update()
+        }
+    }
+}
