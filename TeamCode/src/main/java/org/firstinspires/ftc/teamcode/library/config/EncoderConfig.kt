abdd9dfc02e96ca194ee370d9b705f10856696d0
Dragos Morar <85897880+ProNoob1029@users.noreply.github.com>
2025-02-06 15:11:49 +0200

    reset encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/config/EncoderConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/config/EncoderConfig.kt
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 data class EncoderConfig(
     val deviceName: String,
     val direction: Direction = Direction.FORWARD,
+    val resetEncoder: Boolean = true
 ) {
     enum class Direction {
         FORWARD,
@@ -22,6 +23,11 @@ data class EncoderConfig(
 
     fun createEncoder(hardwareMap: HardwareMap): Encoder {
         val motor = hardwareMap.get(DcMotorEx::class.java, deviceName)
+        if (resetEncoder) {
+            val mode = motor.mode
+            motor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+            motor.mode = mode
+        }
         val encoder = RawEncoder(motor)
         encoder.direction = toEncoderDirection()
         return encoder
