a707387d9c397b632b398eaf07c6ec232154e8f0
ProNoob1029 <pronoob1029@gmail.com>
2025-02-26 18:10:22 +0200

    specimen auto meep

--- MeepMeep/src/main/java/com/meep/meepmeep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/meep/meepmeep/MeepMeep.kt
@@ -10,8 +10,9 @@ import com.lib.units.s
 import com.noahbres.meepmeep.MeepMeep
 import com.noahbres.meepmeep.MeepMeep.Background
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity
 
-data object Red {
+data object RedBasket {
     val startPose = Pose(-36.inch, -60.inch, 90.deg)
     val basketPose = Pose(-54.inch, -54.inch, 45.deg)
     val firstYellowSample = Distance2d(-48.inch, -25.5.inch)
@@ -23,7 +24,7 @@ data object Red {
     val parkPose = Pose(-24.inch, -12.inch, 0.deg)
 }
 
-data object Blue {
+data object BlueBasket {
     val startPose = Pose(36.inch, 60.inch, -90.deg)
     val basketPose = Pose(54.inch, 54.inch, -135.deg)
     val firstYellowSample = Distance2d(48.inch, 25.5.inch)
@@ -33,6 +34,22 @@ data object Blue {
     val thirdYellowSample = Distance2d(68.5.inch, 25.5.inch)
     val thirdYellowPose = Distance2d(56.inch, 50.inch).headingTowards(thirdYellowSample)
     val parkPose = Pose(24.inch, 12.inch, 180.deg)
+}
+
+data object RedSpecimen {
+    val startPose = Pose(4.inch, -60.inch, 90.deg)
+    val firstSpecimenPos = Pose(4.inch, -36.inch, 90.deg)
+    val red1Pos = Distance2d(48.inch, -25.5.inch)
+    val red2Pos = Distance2d(58.5.inch, -25.5.inch)
+    val red3Pos = Distance2d(68.5.inch, -25.5.inch)
+    val zonePos = Distance2d(60.inch, -60.inch)
+    val firstSamplePos = Distance2d(30.inch, -40.inch).headingTowards(red1Pos)
+    val secondSamplePos = Distance2d(34.inch, -40.inch).headingTowards(red2Pos)
+    val thirdSamplePos = Distance2d(38.inch, -40.inch).headingTowards(red3Pos)
+    val firstKickPos = Distance2d(30.inch, -50.inch).headingTowards(zonePos)
+    val secondKickPos = Distance2d(34.inch, -50.inch).headingTowards(zonePos)
+    val thirdKickPos = Distance2d(38.inch, -50.inch).headingTowards(zonePos)
+    val takeSpecimenPos = Pose(34.inch, -56.inch, 90.deg)
 }
 
 fun main() {
@@ -50,43 +67,8 @@ fun main() {
             .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
             .build()
 
-    redBot.runAction(redBot.drive.actionBuilder(Red.startPose.pose2d).ex()
-        .strafeToLinearHeading(Red.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.firstYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.secondYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.thirdYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Red.parkPose)
-        .build()
-    )
-
-    blueBot.runAction(blueBot.drive.actionBuilder(Blue.startPose.pose2d).ex()
-        .strafeToLinearHeading(Blue.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.firstYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.secondYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.thirdYellowPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.basketPose)
-        .waitSeconds(1.s)
-        .strafeToLinearHeading(Blue.parkPose)
-        .build()
-    )
+    //basketAuto(redBot, blueBot)
+    specimenAuto(redBot, blueBot)
 
     meepMeep.setBackground(Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
         .setDarkMode(true)
@@ -94,4 +76,80 @@ fun main() {
         .addEntity(redBot)
         .addEntity(blueBot)
         .start()
+}
+
+fun specimenAuto(redBot: RoadRunnerBotEntity, blueBot: RoadRunnerBotEntity) {
+    redBot.runAction(redBot.drive.actionBuilder(RedSpecimen.startPose.pose2d).ex()
+        .strafeToLinearHeading(RedSpecimen.firstSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.firstSamplePos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.firstKickPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.secondSamplePos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.secondKickPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.thirdSamplePos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.thirdKickPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.takeSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.firstSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.takeSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.firstSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.takeSpecimenPos)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedSpecimen.firstSpecimenPos)
+        .waitSeconds(1.s)
+        .build()
+    )
+
+    blueBot.runAction(blueBot.drive.actionBuilder(BlueBasket.startPose.pose2d).ex()
+        .build()
+    )
+}
+
+fun basketAuto(redBot: RoadRunnerBotEntity, blueBot: RoadRunnerBotEntity) {
+    redBot.runAction(redBot.drive.actionBuilder(RedBasket.startPose.pose2d).ex()
+        .strafeToLinearHeading(RedBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.firstYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.secondYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.thirdYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(RedBasket.parkPose)
+        .build()
+    )
+
+    blueBot.runAction(blueBot.drive.actionBuilder(BlueBasket.startPose.pose2d).ex()
+        .strafeToLinearHeading(BlueBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.firstYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.secondYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.thirdYellowPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.basketPose)
+        .waitSeconds(1.s)
+        .strafeToLinearHeading(BlueBasket.parkPose)
+        .build()
+    )
 }
