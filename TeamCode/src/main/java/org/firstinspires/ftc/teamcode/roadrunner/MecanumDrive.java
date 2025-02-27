7af2f2f632a5259eca3f78c92044cc2c46574034
ProNoob1029 <pronoob1029@gmail.com>
2025-02-27 21:44:34 +0200

    im sick in the bad way

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -272,8 +272,13 @@ public final class MecanumDrive {
         private double beginTs = -1;
 
         private final double[] xPoints, yPoints;
+        private final double correctionTimeSec;
 
         public FollowTrajectoryAction(TimeTrajectory t) {
+            this(t, 0.0);
+        }
+
+        public FollowTrajectoryAction(TimeTrajectory t, double correctionTimeSec) {
             timeTrajectory = t;
 
             List<Double> disps = com.acmerobotics.roadrunner.Math.range(
@@ -286,6 +291,8 @@ public final class MecanumDrive {
                 xPoints[i] = p.position.x;
                 yPoints[i] = p.position.y;
             }
+
+            this.correctionTimeSec = correctionTimeSec;
         }
 
         @Override
@@ -307,7 +314,7 @@ public final class MecanumDrive {
 
             if ((t >= timeTrajectory.duration && error.position.norm() < 1
                     && robotVelRobot.linearVel.norm() < 0.5)
-                    || t >= timeTrajectory.duration + 3) {
+                    || t >= timeTrajectory.duration + correctionTimeSec) {
                 leftFront.setPower(0);
                 leftBack.setPower(0);
                 rightBack.setPower(0);
@@ -488,9 +495,13 @@ public final class MecanumDrive {
     }
 
     public TrajectoryActionBuilder actionBuilder(Pose2d beginPose) {
+        return actionBuilder(beginPose, 0.0);
+    }
+
+    public TrajectoryActionBuilder actionBuilder(Pose2d beginPose, double correctionTimeSec) {
         return new TrajectoryActionBuilder(
                 TurnAction::new,
-                FollowTrajectoryAction::new,
+                (timeTrajectory) -> new FollowTrajectoryAction(timeTrajectory, correctionTimeSec),
                 new TrajectoryBuilderParams(
                         1e-6,
                         new ProfileParams(
