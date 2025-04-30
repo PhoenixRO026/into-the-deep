4dbde88f1b66c9fd8985de748085e56d7fc48bd1
Coman <comanrazvanioan@gmail.com>
2025-04-30 17:03:15 +0300

    got to feedforward step

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/PinpointLocalizer.java
@@ -13,8 +13,8 @@ import java.util.Objects;
 @Config
 public final class PinpointLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = 915.14092294; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = 1810.38747799; // x position of the perpendicular encoder (in tick units)
+        public double parYTicks = 57.5; // y position of the parallel encoder (in tick units)
+        public double perpXTicks = 103.0; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
